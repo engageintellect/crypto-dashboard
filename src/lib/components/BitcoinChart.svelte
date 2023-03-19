767d42ee9b95228cda48ec601b3967f4d38fdc42
engageintellect <jc9361@gmail.com>
2023-03-18 22:55:19 -0700

    cleaning up

--- src/lib/components/BitcoinChart.svelte
+++ src/lib/components/BitcoinChart.svelte
@@ -1,22 +1,25 @@
-<div class="tradingview-widget-container">
-	<div id="tradingview_e91d2" style="height: 80vh;" />
-	<div class="tradingview-widget-copyright" />
-	<script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-	<script type="text/javascript">
-		new TradingView.widget({
-			autosize: true,
-			symbol: 'BNC:BLX',
-			interval: 'W',
-			timezone: 'Etc/UTC',
-			theme: 'dark',
-			style: '1',
-			locale: 'en',
-			toolbar_bg: '#f1f3f6',
-			enable_publishing: false,
-			hide_side_toolbar: false,
-			allow_symbol_change: true,
-			studies: ['BB@tv-basicstudies', 'MACD@tv-basicstudies', 'RSI@tv-basicstudies'],
-			container_id: 'tradingview_e91d2'
-		});
-	</script>
-</div>
+<!-- svelte-ignore a11y-missing-attribute -->
+<html>
+	<div class="tradingview-widget-container">
+		<div id="tradingview_e91d2" style="height: 80vh;" />
+		<div class="tradingview-widget-copyright" />
+		<script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+		<script type="text/javascript">
+			new TradingView.widget({
+				autosize: true,
+				symbol: 'BNC:BLX',
+				interval: 'W',
+				timezone: 'Etc/UTC',
+				theme: 'dark',
+				style: '1',
+				locale: 'en',
+				toolbar_bg: '#f1f3f6',
+				enable_publishing: false,
+				hide_side_toolbar: false,
+				allow_symbol_change: true,
+				studies: ['BB@tv-basicstudies', 'MACD@tv-basicstudies', 'RSI@tv-basicstudies'],
+				container_id: 'tradingview_e91d2'
+			});
+		</script>
+	</div>
+</html>
