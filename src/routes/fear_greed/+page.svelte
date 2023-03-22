dd836a787c857dc3f0be554473e6d293d1c3da70
engageintellect <jc9361@gmail.com>
2023-03-21 21:09:36 -0700

    cleaning up cards

--- src/routes/fear_greed/+page.svelte
+++ src/routes/fear_greed/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import About from '$lib/components/About.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 
@@ -7,14 +6,17 @@
 	const fear_greed_classification = data.fng.data[0].value_classification;
 </script>
 
-<div>
+<div class="px-2">
 	<div class="text-4xl">Fear & Greed</div>
 
+	<!-- ----------------------------------------------- -->
+	<!-- TODAY -->
+	<!-- ----------------------------------------------- -->
 	<div class="text-xl mt-10">Today</div>
 
 	<div class="card">
 		<div class="card-body bg-base-300 my-2">
-			<div class="flex justify-between items-center text-md md:text-lg lg:text-xl">
+			<div class="flex justify-between items-center text-lg md:text-xlg lg:text-2xl">
 				<div class="bg-neutral py-1 px-2 rounded-full">
 					{fear_greed}/100
 				</div>
@@ -33,9 +35,9 @@
 				</div>
 
 				<div class="">
-					{#if fear_greed < 10}
+					{#if fear_greed < 20}
 						<span class="bg-green-600 rounded-full py-1 px-2">BUY🚀</span>
-					{:else if fear_greed > 90}
+					{:else if fear_greed > 80}
 						<span class="bg-red-600 rounded-full py-1 px-2">SELL📉</span>
 					{:else}
 						<span class=" bg-amber-600 rounded-full py-1 px-2">HOLD🕜</span>
@@ -44,41 +46,51 @@
 			</div>
 		</div>
 	</div>
-</div>
 
-<div>
 	<div class="text-center">
 		<a href="/about"> What is the Fear & Greed Index?</a>
 	</div>
-</div>
 
-<div class="mt-10">
-	<div class="text-xl">Last 30 Days</div>
+	<!-- ----------------------------------------------- -->
+	<!-- LAST 7 DAYS -->
+	<!-- ----------------------------------------------- -->
 
-	<div class="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-2 md:gap-5">
-		{#each data.last_30_days.data as day}
-			<div class="flex flex-col bg-base-100 p-2 gap-2 shadow-md">
-				<div>
-					{#if day.calue_classification === 'Extreme Fear'}
-						<span class="bg-green-900 rounded-full py-1 px-2">{day.value_classification}</span>
-					{:else if day.value_classification === 'Fear'}
-						<span class="bg-green-600 rounded-full py-1 px-2">{day.value_classification}</span>
-					{:else if day.value_classification === 'Neutral'}
-						<span class="bg-amber-600 rounded-full py-1 px-2">{day.value_classification}</span>
-					{:else if day.value_classification === 'Greed'}
-						<span class="bg-red-600 rounded-full py-1 px-2">{day.value_classification}</span>
-					{:else if day.value_classification === 'Extreme Greed'}
-						<span class="bg-red-900 rounded-full py-1 px-2">{day.value_classification}</span>
-					{/if}
-					<!-- {day.value_classification} -->
+	<!-- ----------------------------------------------- -->
+	<!-- LAST 30 DAYS -->
+	<!-- ----------------------------------------------- -->
+	<div class="mt-10">
+		<div class="text-xl">Last 30 Days</div>
+
+		<div class="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-2 sm:gap-5">
+			{#each data.last_30_days.data as day}
+				<div class="flex flex-col bg-base-100 p-2 gap-2 shadow-md">
+					<div>
+						{#if day.calue_classification === 'Extreme Fear'}
+							<span class="bg-green-900 rounded-full py-1 px-2">BUY</span>
+						{:else if day.value_classification === 'Fear'}
+							<span class="bg-green-600 rounded-full py-1 px-2">BUY</span>
+						{:else if day.value_classification === 'Neutral'}
+							<span class="bg-amber-600 rounded-full py-1 px-2">HOLD</span>
+						{:else if day.value_classification === 'Greed'}
+							<span class="bg-red-600 rounded-full py-1 px-2">HOLD</span>
+						{:else if day.value_classification === 'Extreme Greed'}
+							<span class="bg-red-900 rounded-full py-1 px-2">SELL</span>
+						{/if}
+						<!-- {day.value_classification} -->
+					</div>
+					<div>
+						{day.value}/100
+					</div>
+					<div class="font-thin text-sm">
+						{new Date(day.timestamp * 1000).toLocaleDateString('en-US', {
+							weekday: 'short',
+							year: '2-digit',
+							month: '2-digit',
+							day: '2-digit'
+						})}
+					</div>
 				</div>
-				<div>
-					{day.value}/100
-				</div>
-				<div>
-					{new Date(day.timestamp * 1000).toLocaleDateString()}
-				</div>
-			</div>
-		{/each}
+			{/each}
+		</div>
 	</div>
 </div>
--- src/routes/fear_greed/+page.ts
+++ src/routes/fear_greed/+page.ts
@@ -1,10 +1,12 @@
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch, params }) => {
+	// TODO: These two fetches are identical, except for the limit parameter. Can we make this more DRY?
 	const res = await fetch(`https://api.alternative.me/fng/?limit=6`);
 	const fng = await res.json();
 
 	const last_30_days_res = await fetch(`https://api.alternative.me/fng/?limit=30`);
 	const last_30_days = await last_30_days_res.json();
+
 	return { fng, last_30_days };
 }) satisfies PageLoad;
